865a90c642fc479f3eb7f632d87ef01864e5143f
Youngsung Kim <youngsung.kim.act2@gmail.com>
2020-05-20 20:50:50 -0400

    added parsemk app

---
+++ tests/test_basic.py
@@ -0,0 +1,41 @@
+import os
+from langlab import Langlab
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+
+def test_basic():
+
+    prj = Langlab()
+
+    cmd = "input @1 --forward '@x=2'"
+    ret = prj.main(cmd)
+
+    assert ret == 0
+
+def test_print(capsys):
+
+    prj = Langlab()
+
+    cmd = "-- input @1 --forward '@x=2' -- print @x @data[0]"
+    ret = prj.main(cmd)
+
+    assert ret == 0
+
+    captured = capsys.readouterr()
+    assert captured.out == "21\n"
+    assert captured.err == ""
+
+def test_parse_makefile(capsys):
+
+    prj = Langlab()
+    makefile = os.path.join(here, "src", "Depends.intel")
+
+    cmd = "-- parsemk %s -s -- print @data" % makefile
+    ret = prj.main(cmd)
+
+    assert ret == 0
+
+    captured = capsys.readouterr()
+    assert captured.out.startswith("PERFOBJS")
+    assert captured.err == ""
